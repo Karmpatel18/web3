d8f0de8f4d7db115c5ee6558186b7048d29156ee
Karmpatel18 <karmpatel1203@gmail.com>
2025-04-24 15:00:29 +0530

    sol faucet

--- solana_adapter/src/App.jsx
+++ solana_adapter/src/App.jsx
@@ -9,17 +9,20 @@ import {
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import '@solana/wallet-adapter-react-ui/styles.css';
+import Input from './components/Input';
 
 function App() {
   
 
   return (
     <div>
-      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <ConnectionProvider endpoint={"https://rough-proportionate-shadow.solana-devnet.quiknode.pro/4bf584907bb8f4013c042f72aa6a4e319a5e05b8/"}>
             <WalletProvider wallets={[]} autoConnect>
                 <WalletModalProvider>
+                    <div className="flex flex-col w-full justify-center h-screen items-center">
                     <WalletMultiButton />
-                    
+                    <Input/>
+                    </div>
                     
                 </WalletModalProvider>
             </WalletProvider>
